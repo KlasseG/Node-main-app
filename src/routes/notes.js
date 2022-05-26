8ea5d383b5a0c64de5860d3a5e3a6b1d050f5534
RodrigoFA216 <rfloresa1900@alumno.ipn.mx>
2022-05-26 14:50:17 -0500

    Crea y muestra notas (Error con handlebars)

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,8 +1,38 @@
 const express= require('express');
 const router= express.Router();
 
-router.get('/notes', (req, res)=>{
-    res.send('Notas de la base de datos');
+const Note= require ('../models/Note');
+
+router.get('/notes/add', (req, res)=>{
+    res.render('notes/new-note');
+});
+
+router.post('/notes/new-note', async (req, res)=>{
+    const {title, description}=req.body;
+    const errors=[];
+    if (!title) {
+        errors.push({text: 'Por favor introduzca un Nombre para la materia'});
+    }
+    if (!description) {
+        errors.push({text: 'Por favor introduzca una descripción'});
+    }
+    if (errors.length >0) {
+        res.render('notes/new-note', {
+            errors,
+            title, 
+            description
+        });
+    } else {
+        const newNote= new Note({title, description});
+        // console.log(newNote);
+        await newNote.save();
+        res.redirect('/notes');
+    }
+})
+
+router.get('/notes', async(req, res)=>{
+    const notes=await Note.find().sort({date: 'desc'});
+    res.render('notes/all-notes', {notes});
 })
 
 module.exports=router;
